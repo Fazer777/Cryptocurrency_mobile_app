f374a00f0604c0fbd8fead28c9989ecfa02e1689
msoup <m.soup2016@yandex.ru>
2024-08-18 12:27:24 +0300

    MainActivity with custom toolbar has been created

---
+++ app/src/main/java/com/project/cryptocurrencyapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.project.cryptocurrencyapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project.cryptocurrencyapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.actToolbar)
+    }
+}
